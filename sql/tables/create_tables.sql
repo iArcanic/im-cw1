ffe7d7ccd07235037eb1fbc6f9513d9168afc0b1
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-08 18:29:43 +0000

    Added Table definition

--- sql/tables/create_tables.sql
+++ sql/tables/create_tables.sql
@@ -17,3 +17,14 @@ CREATE TABLE Account (
 	balance DECIMAL(10, 2) DEFAULT 0.0,
 	status VARCHAR(20) DEFAULT 'Active'
 );
+
+-- Create Transaction table
+CREATE TABLE Transaction (
+	transaction_id SERIAL PRIMARY KEY,
+	account_id SERIAL REFERENCES Account(account_id),
+	manager_id SERIAL REFERENCES Manager(manager_id),
+	game_id SERIAL REFERENCES Game(game_id),
+	amount DECIMAL(10, 2),
+	status VARCHAR(20),
+	timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
