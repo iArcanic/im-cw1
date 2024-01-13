49d1bff128e9abd31679b77d89737c13da158367
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-13 20:57:31 +0000

    Add get_transaction_count function

--- sql/views-functions/create_functions.sql
+++ sql/views-functions/create_functions.sql
@@ -13,3 +13,19 @@ INT) RETURNS DECIMAL AS
 	RETURN total_balance;
 	END $$ LANGUAGE
 plpgsql; 
+
+-- Function to get the count of transactions for a player
+CREATE OR REPLACE FUNCTION get_transaction_count(player_id 
+INT) RETURNS INT AS 
+	$$ DECLARE transaction_count INT;
+	BEGIN
+	SELECT
+	    COUNT (*) INTO transaction_count
+	FROM Transaction t
+	    JOIN Account a ON t.account_id = a.account_id
+	WHERE
+	    a.player_id = get_transaction_count.player_id;
+	RETURN transaction_count;
+	END;
+	$$ LANGUAGE
+plpgsql; 
