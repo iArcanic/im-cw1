8b0f6f41e80994899383ae6e7a54e52db4316f2e
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-13 20:49:30 +0000

    Add get_total_balance function

--- sql/views-functions/create_functions.sql
+++ sql/views-functions/create_functions.sql
@@ -1,1 +1,15 @@
 -- im-cw1/sql/views-functions/create_functions.sql
+
+-- Function to calculate the total balance of a player's account
+CREATE OR REPLACE FUNCTION get_total_balance(player_id 
+INT) RETURNS DECIMAL AS 
+	$$ DECLARE total_balance DECIMAL;
+	BEGIN
+	SELECT
+	    SUM (balance) into total_balance
+	FROM Account
+	WHERE
+	    player_id = get_total_balance.player_id;
+	RETURN total_balance;
+	END $$ LANGUAGE
+plpgsql; 
