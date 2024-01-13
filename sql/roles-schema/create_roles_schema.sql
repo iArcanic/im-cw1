10ecf2cf58980ea2c222e09046d5ec6b45e50b03
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-13 19:21:47 +0000

    Added schema definitions and relevant table privilege to roles

--- sql/roles-schema/create_roles_schema.sql
+++ sql/roles-schema/create_roles_schema.sql
@@ -14,22 +14,83 @@ GRANT ALL PRIVILEGES ON SCHEMA public TO admin;
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE employee TO manager;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Employee TO manager;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE manager TO manager;
+GRANT
+SELECT (
+        manager_id,
+        employee_id,
+        name,
+        date_of_birth,
+        address
+    ) ON
+TABLE Manager TO manager;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE transaction TO manager;
+GRANT
+UPDATE (
+        employee_id,
+        name,
+        date_of_birth,
+        address
+    ) ON
+TABLE Manager TO manager;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE player TO manager;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Transaction TO manager;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE player TO player;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Player TO manager;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE transaction TO player;
+GRANT
+SELECT (
+        player_id,
+        name,
+        date_of_birth,
+        email,
+        address,
+        username
+    ) ON
+TABLE Player TO player;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE account TO player;
+GRANT
+UPDATE (
+        name,
+        date_of_birth,
+        email,
+        address,
+        username
+    ) ON
+TABLE Player TO player;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE player TO employee;
+GRANT SELECT (player_id, status) ON TABLE Account TO player;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE game TO employee;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Player TO employee;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE employee TO employee;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Game TO employee;
+
+-- Create schemas
+CREATE SCHEMA game_schema AUTHORIZATION manager;
+
+CREATE SCHEMA player_schema AUTHORIZATION player;
+
+CREATE SCHEMA employee_schema AUTHORIZATION employee;
+
+-- Object ownership
+ALTER TABLE game_schema.Game OWNER TO manager;
+
+ALTER TABLE player_schema.Player OWNER TO player;
+
+ALTER TABLE employee_schema.Employee OWNER TO employee;
+
+-- Default privileges
+ALTER DEFAULT PRIVILEGES IN SCHEMA game_schema
+GRANT
+SELECT,
+INSERT,
+UPDATE,
+DELETE ON TABLES TO manager;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA player_schema
+GRANT
+SELECT,
+INSERT,
+UPDATE,
+DELETE ON TABLES TO player;
