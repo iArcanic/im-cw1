18881477de0dfe54e4caf978a3c48146c7ca979b
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-20 13:02:11 +0000

    Corrected Player CREATE TABLE SQL statement

--- master.sql
+++ master.sql
@@ -12,7 +12,7 @@ CREATE TABLE Employee (
 
 -- Create Player table
 CREATE TABLE Player (
-    player_id SERIAL PRIMARY KEY, employee_id SERIAL REFERENCES Employee (employee_id), game_id SERIAL REFERENCES Game (game_id), name VARCHAR(255) NOT NULL, date_of_birth DATE, email VARCHAR(255) UNIQUE, address TEXT username VARCHAR(50) UNIQUE
+    player_id SERIAL PRIMARY KEY, employee_id SERIAL REFERENCES Employee (employee_id), game_id SERIAL REFERENCES Game (game_id), name VARCHAR(255) NOT NULL, date_of_birth DATE, email VARCHAR(255) UNIQUE, address TEXT, username VARCHAR(50) UNIQUE
 );
 
 -- Create Manager table
