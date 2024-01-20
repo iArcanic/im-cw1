45bd7ca278357a77378dbc0140504e863e5537ab
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-01-20 13:07:43 +0000

    Reordered Account CREATE TABLE SQL statement

--- master.sql
+++ master.sql
@@ -35,6 +35,11 @@ CREATE TABLE EmployeeGame (
     employee_id SERIAL REFERENCES Employee (employee_id), game_id SERIAL REFERENCES Game (game_id), PRIMARY KEY (employee_id, game_id)
 );
 
+-- Create Account table
+CREATE TABLE Account (
+account_id SERIAL PRIMARY KEY, player_id SERIAL REFERENCES Player (player_id), balance DECIMAL(10, 2) DEFAULT 0.0, status VARCHAR(20) DEFAULT 'Active'
+);
+
 -- Create Transaction table
 CREATE TABLE Transaction (
     transaction_id SERIAL PRIMARY KEY, account_id SERIAL REFERENCES Account (account_id), manager_id SERIAL REFERENCES Manager (manager_id), game_id SERIAL REFERENCES Game (game_id), amount DECIMAL(10, 2), status VARCHAR(20), timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -43,11 +48,6 @@ CREATE TABLE Transaction (
 -- Create TransactionGame table
 CREATE TABLE TransactionGame (
     transaction_id SERIAL REFERENCES Transaction (transaction_id), game_id SERIAL REFERENCES Game (game_id), PRIMARY KEY (transaction_id, game_id)
-);
-
--- Create Account table
-CREATE TABLE Account (
-    account_id SERIAL PRIMARY KEY, player_id SERIAL REFERENCES Player (player_id), balance DECIMAL(10, 2) DEFAULT 0.0, status VARCHAR(20) DEFAULT 'Active'
 );
 --
 -- -- Create relationships
